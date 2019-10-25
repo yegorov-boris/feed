0b570763b99370cf2dcd6804f955b8d14daa5e6a
b.egorov <b.egorov@tinkoff.ru>
2019-10-25 17:45:49 +0300

    Use hyper_router

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,40 @@
 extern crate hyper;
-extern crate futures;
+extern crate hyper_router;
 
-use futures::future;
-use hyper::rt::{Future, Stream};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use hyper::service::service_fn;
+use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use hyper::{Request, Response, Body, Method};
+use hyper::server::Server;
+use hyper::rt::Future;
+use hyper_router::{Route, RouterBuilder, RouterService};
 
-type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
-
-fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
-
-    let server = Server::bind(&addr)
-        .serve(|| service_fn(echo))
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    println!("Listening on http://{}", addr);
-    hyper::rt::run(server);
+fn request_handler(_: Request<Body>) -> Response<Body> {
+    let body = "Hello World";
+    Response::builder()
+        .header(CONTENT_LENGTH, body.len() as u64)
+        .header(CONTENT_TYPE, "text/plain")
+        .body(Body::from(body))
+        .expect("Failed to construct the response")
 }
 
-fn echo(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
+fn router_service() -> Result<RouterService, std::io::Error> {
+    let router = RouterBuilder::new()
+        .add(Route::get("/posts").using(request_handler))
+        .add(Route::post("/posts").using(request_handler))
+        .add(Route::put("/posts/\\d+").using(request_handler))
+        .add(Route::delete("/posts/\\d+").using(request_handler))
+        .add(Route::post("/profiles").using(request_handler))
+        .add(Route::put("/profiles/\\d+").using(request_handler))
+        .add(Route::delete("/profiles/\\d+").using(request_handler))
+        .build();
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/posts") => {
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
-        }
+    Ok(RouterService::new(router))
+}
 
-        (&Method::POST, "/posts") => {
-            *response.body_mut() = req.into_body();
-        }
+fn main() {
+    let addr = "127.0.0.1:8080".parse().unwrap();
+    let server = Server::bind(&addr)
+        .serve(router_service)
+        .map_err(|e| eprintln!("server error: {}", e));
 
-        (&Method::PUT, "/posts") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        (&Method::DELETE, "/posts") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        (&Method::PUT, "/profiles") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        (&Method::DELETE, "/profiles") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    };
-
-    Box::new(future::ok(response))
+    hyper::rt::run(server)
 }
